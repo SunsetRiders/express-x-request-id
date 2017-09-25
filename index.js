0d6483840e0aafb9f2b9205e3ed8cc35760a4f05
Leandro Curioso <leandro.curioso@gmail.com>
2017-09-25 11:00:45 -0300

    Added .eslintrc and new approach to bind X-Request-Id into request and response objects.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const uuid = require("uuid");
+const uuid = require('uuid');
 
 /**
  * This class provide a simple middleware
- * to set a X-Request-Id UUID token into response header.
- * this header can be accessed with res._headers['x-request-id']
+ * to set a X-Request-Id UUID token into request and response header.
  */
 class ExpressXRequestId {
-  
   /**
-   * Express middleware
+   * Express request middleware
    * @param {Object} req Request object
    * @param {Object} res Response object
    * @param {Function} next Next function
    */
-  static middleware(req, res, next) {
-    let requestId = req.get('X-Request-Id') || uuid();
-    requestId = requestId || res._headers['x-request-id'];
-    res.set("X-Request-Id", requestId);
+  static requestMiddleware(req, res, next) {
+    req.xRequestId = req.get('X-Request-Id') || uuid();
     next();
   }
 
+    /**
+   * Express response middleware
+   * @param {Object} req Request object
+   * @param {Object} res Response object
+   * @param {Function} next Next function
+   */
+  static responseMiddleware(req, res, next) {
+    res.set('X-Request-Id', req.get('X-Request-Id'));
+    next();
+  }
 }
 
 module.exports = ExpressXRequestId;
